941150272f7ca4806aab5f6b899b4a48fb2496f4
Matej Bádal <matej.badal@profesia.sk>
2023-04-07 17:25:14 +0200

    Implemented an infrastructure for translation of a received message to command

---
+++ src/Application/Command/Exception/CommandClassDoesNotExistException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Profesia\DddBackbone\Application\Command\Exception;
+
+use Profesia\DddBackbone\Application\Exception\AbstractApplicationException;
+
+final class CommandClassDoesNotExistException extends AbstractApplicationException
+{
+
+}
---
+++ src/Application/Command/Factory/EventCommandFactory.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Profesia\DddBackbone\Application\Command\Factory;
+
+use Profesia\DddBackbone\Application\Command\CommandInterface;
+use Profesia\DddBackbone\Application\Command\Exception\CommandClassDoesNotExistException;
+use Profesia\DddBackbone\Application\Command\Exception\NoCommandRegisteredForEventTypeException;
+use Profesia\MessagingCore\Broking\Dto\ReceivedMessage;
+
+class EventCommandFactory
+{
+    /** @var string[] */
+    private array $eventCommandMap = [];
+
+    public function registerCommandClass(string $eventType, string $commandClass): self
+    {
+        $this->eventCommandMap[$eventType] = $commandClass;
+
+        return $this;
+    }
+
+    public function createFromReceivedMessage(ReceivedMessage $receivedMessage): CommandInterface
+    {
+        $eventType = $receivedMessage->getEventType();
+        if (array_key_exists($eventType, $this->eventCommandMap) === false) {
+            throw new NoCommandRegisteredForEventTypeException("No command registered for event type: [{$eventType}]");
+        }
+
+        if (class_exists($this->eventCommandMap[$eventType]) === false) {
+            throw new CommandClassDoesNotExistException("Command class: [{$this->eventCommandMap[$eventType]}] does not exist");
+        }
+
+        return call_user_func(
+            [
+                $this->eventCommandMap[$eventType],
+                'createFromJsonString'
+            ],
+            $receivedMessage->decodePayload()
+        );
+    }
+}
